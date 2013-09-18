34ee4bb5904edc2264a9e20e6f17cecf11abfb1c
Ivan De Marino <ivan.de.marino@gmail.com>
2013-09-18 18:03:32 +0100

    Adding "Match" (with tests).
    This is a logical representation of a match between
    2 players.
    It keeps track of the number of players, maintaining a useful
    status to know at which "point" in the game we are.
    
    It will be useful to represent the concept of "Room" where max 2 players are allowed
    to join and play.

---
+++ src/test/java/com/github/detro/rps/test/MatchTest.java
@@ -0,0 +1,119 @@
+package com.github.detro.rps.test;
+
+import com.github.detro.rps.Match;
+import com.github.detro.rps.Weapons;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertNotNull;
+
+public class MatchTest {
+
+    @DataProvider(name = "matchAndResultProvider")
+    public Object[][] matchAndResultProvider() {
+        return new Object[][] {
+                { "player1ID", 1, "player2ID", 0, "player1ID" },
+                { "player1ID", 1, "player2ID", 2, "player2ID" },
+                { "player1ID", 2, "player2ID", 2, "draw" },
+                { "player1ID", 0, "player2ID", 2, "player1ID" },
+                { "player1ID", 1, "player2ID", 1, "draw" },
+                { "player1ID", 0, "player2ID", 0, "draw" },
+        };
+    }
+
+    @Test(dataProvider = "matchAndResultProvider")
+    public void shouldReturnTheCorrectMatchResult(
+            String player1id, int player1weapon,
+            String player2id, int player2weapon,
+            String winner) {
+
+        Match match = new Match(player1id + player2id);
+        assertNotNull(match.getId());
+        assertEquals(match.playersAmount(), 0);
+        assertEquals(match.getStatus(), Match.NO_PLAYERS_YET);
+
+        match.addPlayer(player1id);
+        assertEquals(match.playersAmount(), 1);
+        assertEquals(match.getStatus(), Match.WAITING_FOR_ANOTHER_PLAYER);
+        assertTrue(match.containsPlayer(player1id));
+
+        match.addPlayer(player2id);
+        assertEquals(match.playersAmount(), 2);
+        assertEquals(match.getStatus(), Match.WAITING_PLAYERS_WEAPONS);
+
+        match.setPlayerWeapon(player2id, player2weapon);
+        assertEquals(match.getStatus(), Match.WAITING_PLAYERS_WEAPONS);
+        match.setPlayerWeapon(player1id, player1weapon);
+        assertEquals(match.getStatus(), Match.PLAYED);
+
+        assertEquals(match.getWinner(), winner);
+    }
+
+    @Test(dataProvider = "matchAndResultProvider")
+    public void shouldReturnTheCorrectMatchResultAfterReset(
+            String player1id, int player1weapon,
+            String player2id, int player2weapon,
+            String winner) {
+
+        Match match = new Match(player1id + player2id);
+
+        // first match
+        match.addPlayer(player1id);
+        match.addPlayer(player2id);
+        assertEquals(match.getStatus(), Match.WAITING_PLAYERS_WEAPONS);
+        match.setPlayerWeapon(player2id, player2weapon);
+        match.setPlayerWeapon(player1id, player1weapon);
+        assertEquals(match.getStatus(), Match.PLAYED);
+        assertEquals(match.getWinner(), winner);
+
+        // reset match
+        match.reset();
+        assertEquals(match.getStatus(), Match.WAITING_PLAYERS_WEAPONS);
+        match.setPlayerWeapon(player1id, player1weapon);
+        match.setPlayerWeapon(player2id, player2weapon);
+        assertEquals(match.getStatus(), Match.PLAYED);
+        assertEquals(match.getWinner(), winner);
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void shouldNotAllowToAddSamePlayerTwice() {
+        Match match = new Match("seed");
+        match.addPlayer("player");
+        match.addPlayer("player");
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void shouldNotAllowToAddMoreThan2Players() {
+        Match match = new Match("seed");
+        match.addPlayer("player1");
+        match.addPlayer("player2");
+        match.addPlayer("player3");
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void shouldNotAllowPlayerNotInTheMatchToSetTheirWeapon() {
+        Match match = new Match("seed");
+        match.addPlayer("player1");
+        match.addPlayer("player2");
+
+        match.setPlayerWeapon("player3", Weapons.pickRandomWeapon());
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void shouldNotAllowToSetPlayerWeaponBefore2PlayersHaveBeenAdded() {
+        Match match = new Match("seed");
+        match.addPlayer("player1");
+        match.setPlayerWeapon("player1", Weapons.pickRandomWeapon());
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void shouldNotAllowToGetMatchResultBeforeBothPlayersHaveSetTheirWeapon() {
+        Match match = new Match("seed");
+        match.addPlayer("player1");
+        match.addPlayer("player2");
+        match.setPlayerWeapon("player1", Weapons.pickRandomWeapon());
+        match.getWinner();
+    }
+}
