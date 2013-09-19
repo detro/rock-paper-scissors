5a9efc5bf085c4d57dd6b5181c06e0587666818d
Ivan De Marino <ivan.de.marino@gmail.com>
2013-09-19 14:24:27 +0100

    Fixing tests for "Game Center".
    Using the same "seed" creates issue when creating Matches:
    if the tests are too fast, Match.id generated would
    be the same (i.e. same seed + same timestamp).

--- src/test/java/com/github/detro/rps/test/GameCenterTest.java
+++ src/test/java/com/github/detro/rps/test/GameCenterTest.java
@@ -14,14 +14,14 @@ public class GameCenterTest {
         GameCenter gCenter = new GameCenter();
 
         // Add 1 match and check it's there
-        Match match = new Match("seed");
+        Match match = new Match("seed1");
         gCenter.addMatch(match);
         assertTrue(gCenter.containsMatch(match.getId()));
         assertEquals(gCenter.getMatch(match.getId()), match);
         assertEquals(gCenter.getMatches().size(), 1);
 
         // Add another match and check it's there as well
-        match = new Match("seed");
+        match = new Match("seed2");
         gCenter.addMatch(match);
         assertTrue(gCenter.containsMatch(match.getId()));
         assertEquals(gCenter.getMatch(match.getId()), match);
@@ -33,22 +33,22 @@ public class GameCenterTest {
         GameCenter gCenter = new GameCenter();
 
         // Add 1 match without any user
-        Match match = new Match("seed");
+        Match match = new Match("seed1");
         gCenter.addMatch(match);
 
         // Add 1 match with a player "player1Id"
-        match = new Match("seed");
+        match = new Match("seed2");
         match.addPlayer("player1Id");
         gCenter.addMatch(match);
 
         // Add 1 match with 2 players, one of which is also in another match
-        match = new Match("seed");
+        match = new Match("seed3");
         match.addPlayer("player1Id");
         match.addPlayer("player2Id");
         gCenter.addMatch(match);
 
         // Add 1 match with 2 players that are in no other match
-        match = new Match("seed");
+        match = new Match("seed4");
         match.addPlayer("player3Id");
         match.addPlayer("player4Id");
         gCenter.addMatch(match);
